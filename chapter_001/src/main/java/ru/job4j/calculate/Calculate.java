44399c48f1d6fb200fe82e3b09c7debeb6cfb9f6
yankovski88 <boets003@mail.ru>
2018-09-08 10:34:17 +0300

    1.2. Подключить сборщик Maven[#40866]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
-﻿package ru.job4j.calculate;
+package ru.job4j.calculate;
 
+
+/**
+* Package for calculate task.
+*
+* @author Yura Yankovski (mailto:@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
 public class Calculate {
 
     public static void main(String[] args){
