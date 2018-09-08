2b030e0fd60483a4df859f7497b10dcfbd7ef513
yankovski88 <boets003@mail.ru>
2018-09-08 23:03:02 +0300

    2.5. Инспектор кода IDEA. [#69757]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,32 +2,33 @@ package ru.job4j.calculate;
 
 
 /**
-* Package for calculate task.
+ * Package for calculate task.
 
-* @author Yura Yankovski (mailto:@mail.ru).
-* @version $Id$.
-* @since 0.1
-*/
-public class Calculate {
-	
-/**
- * Method main
- * @param args array of strings
+ * @author Yura Yankovski (mailto:@mail.ru).
+ * @version $Id$.
+ * @since 0.1
  */
+public class Calculate {
+
+    /**
+     * Method main
+     * @param args array of strings
+     */
     public static void main(String[] args) {
-		
+
 
         System.out.println("Hello World");
-		
-		
+
+
 
     }
-	/**
-* Method echo.
-* @param name yura yankovski.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-		}
+    /**
+     * Method echo.
+     * @param name yura yankovski.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+
+        return "Echo, echo, echo : " + name;
+    }
 }
