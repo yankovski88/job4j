9a46eb16d5fcceae362462d8c1afca5ceb41ae4f
yankovski88 <boets003@mail.ru>
2018-09-09 16:13:44 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,6 +8,36 @@ public class Calculator {
         this.result = first + second;
     }
 
+
+    /**
+     * division Деление
+     * @param first делимое
+     * @param second делитель
+     */
+    public void division (double first, double second) {
+        this.result = first / second;
+    }
+    /**
+     * умножение
+     * @param first
+     * @param second
+     */
+
+    public void multiplication (double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * вычетание
+     * @param first
+     * @param second
+     */
+    public void vichet (double first, double second) {
+        this.result = first - second;
+    }
+    /**
+     * получить результат
+     */
     public double getResult () {
         return this.result;
     }
