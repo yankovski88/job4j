f3dda7ba3385e5779b0773b8d8619fd36e1fb590
yankovski88 <boets003@mail.ru>
2018-09-12 21:03:00 +0300

    3.4. Расстояние между точками в системе координат [#188][#70486]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,42 +1,17 @@
 package ru.job4j.condition;
 
-/**
- * В этом классе мы создали поля.
- *
- * @author yura yankovski mailto:
- * @version $Id$
- * @since 0.1
- *
- */
-/*
 public class Point {
-    public int x;
-    public int y;
-    /**
-     *
-     * и конструктор.
-     */
-/*
-    public Point (int x, int y) {
+    private int x;
+    private int y;
+
+    public  Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    /**
-     *измеряет расстояние
-     */
-    /*
-    public double distanceTo (Point that) {
-        // Точка А - это текущая точка. К ней мы обращаемся через оператор this.
-       // Point a = this;
-        // Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
-        // или для удобства мы создали новую переменную b и к ней присвоили переменную that.
-        //Point b = that;
-        // сделаем вывод на консоль.
-        //int x1 = a.x;
-        //int y1 = a.y;
-        //int x2 = b.x;
-        //int y2 = b.y;
-       return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
         );
     }
 
@@ -53,12 +28,5 @@ public class Point {
         System.out.println("Расстояние между точками А и В : " + result);
     }
 
-    public double getResult() {
-        return result;
-    }
 
-    public void setResult(double result) {
-        this.result = result;
-    }
 }
-*/
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,22 +1,21 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-import ru.job4j.calculator.Calculator;
-
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 
 public class PointTest {
-    @Test // нужно поставить для автоматического теста
-    // ругается javaDoc на комент ниже
-
-    public void whenPointZeroOneTwoFiveThenFourFour () { // обозвать when что делаем Then что ждем
-        Point poin = new Point(0, y: 1); //
-        poin.distanceTo (2, y: 2); //
-        double result = poin.getResult(); //
-        double expected_division = 4.4; // enter our waiting result
-        assertThat (result, is(expected_division)); // have comparison (сравнение)
-
+    /**
+     * Тест проверяющий работу метода вычисляющего расстояние между
+     * точками
+     */
+    @Test
+    public void whenPointZeroOneTwoFiveThenFourFour() { // обозвать when что делаем Then что ждем
+        Point a = new Point(0, 1);// создаем объект первой точки
+        Point b = new Point(2, 5);//создаем  объект второй точки
+        double dist = a.distanceTo(b);//вычисляем расстояние и сохраняем его в переменную dist
+        double expected = 4.47213595499958; // ожидаемый результат
+        assertThat(dist, is(expected )); //сравниваем то что получили с ожидаемым результатом
     }
 }
-
