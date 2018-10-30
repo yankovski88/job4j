c9eb9dd081d690d808a31e42bcc006b45b608dac
yankovski88 <boets003@mail.ru>
2018-10-30 20:59:36 +0300

    5.1. Подсчет суммы чётных чисел в [192][#81160]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -2,23 +2,22 @@ package ru.job4j.loop;
 
 
 public class Counter {
-
-//  static  ArrayList<Integer> list = new ArrayList<Integer>();
-
+    /**
+     * Counter.add(start, finish) возвращает сумму всех четных чисел
+     * @param start начало диапазона
+     * @param finish конец диапазона
+     * @author yura yankovski(mailto:boets003@mail.ru)
+     * @version 1.
+     */
     public static int add(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-    if ( i % 2 == 0){
-        sum = sum + i;
-    }
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
         }
-//        list.add(sum);
-//        System.out.println(list);
         return sum;
     }
-//    public static void main(String args[]) {
-//        add(1, 10);
-//    }
 }
 
-   //// Для диапазона значений от 1 до 10, метод add должен вернуть сумму чётных чисел, равную 30 (2 + 4 + 6 + 8 + 10).
+//// Для диапазона значений от 1 до 10, метод add должен вернуть сумму чётных чисел, равную 30 (2 + 4 + 6 + 8 + 10).
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,13 +1,17 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CounterTest {
     @Test
-
-    public void whenEnterInAddStartFinishThenSumChetNumber(){
+/**
+ * proverka суммы на заданый диапазон.
+ * @author Yura Yankovski (mailto:boets003@mail.ru)
+ */
+    public void whenEnterInAddStartFinishThenSumChetNumber() {
         int start = 1;
         int finish = 10;
         int expect = 30;
@@ -15,34 +19,4 @@ public class CounterTest {
         int result = counter.add(start, finish);
         assertThat(result, is(expect));
     }
-
 }
-//    @Test
-//    public void manWeight() {
-//        Fit fit = new Fit();
-//        double weight = fit.manWeight(180);
-//        assertThat(weight, closeTo(92.0, 0.1));
-//        /*
-//        В этом тесты мы используем метод closeTo - Он нужен что сравнения чисел с плавающей точкой.
-//closeTo(value, round)
-//value - это ожидаемое значение
-//round - это диапазон в округ значения. например, если round = 0.1, то диапазон будет [value - 0.1, value + 0.1];
-//Ожидаемое значение должно попасть в этот диапазон.
-//         */
-//    }
-
-
-//public class CalculateTest {
-//    ///**
-//     * Test echo.
-//     */
-//    @Test
-//    public void whenTakeNameThenTreeEchoPlusName() {
-//        String input = "Petr Arsentev";
-//        String expect = "Echo, echo, echo : Petr Arsentev";
-//        Calculate calc = new Calculate();
-//        String result = calc.echo(input);
-//        assertThat(result, is(expect));
-//    }
-//
-//}
