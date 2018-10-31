ceb84bed7fea448d824f1df9548e7bf3927e255f
yankovski88 <boets003@mail.ru>
2018-10-31 13:49:35 +0300

    4.4. Максимум из трех чисел[#81117]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,11 +1,24 @@
 package ru.job4j.max;
 
 /**
- *выодит большее число
+ * выодит большее число из 2 введенных
  */
 public class Max {
-    public int biger (int first, int second) {
-        return  first > second ? first : second;
+    public int biger(int first, int second) {
+        return first > second ? first : second;
 
     }
+
+    /**
+     * выбор максимльного числа из 3 введеных
+     *
+     * @author Yura Yankovski (mailto:boets003@mail.ru)
+     */
+    public int maxthird(int first, int second, int third) {
+        return biger(third, biger(first, second));
+    }
+//    public int max(int first, int second){
+//        int temp = this.max(first, second);
+//        return temp;
+//    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -13,7 +13,19 @@ public class MaxTest {
         Max number = new Max ();
         int big_number = number.biger(1, 2);
         assertThat(big_number, is(2));
+    }
 
-
+    @Test
+    /**
+     * выбор максимального числа из 3 введеных чисел
+     * @author Yura Yankovski (mailto:boets003@mail.ru)
+     */
+    public void whenEnterThreeNumberThenViborMax(){
+        Max numb = new Max();
+        int expect = 3;
+        int max_num = numb.maxthird(1,2,3);
+        assertThat(max_num, is(expect));
     }
 }
+
+
