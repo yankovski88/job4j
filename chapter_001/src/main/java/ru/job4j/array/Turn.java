679ef0aa6ab3ec59d73ad34715e9c50d456c724a
yankovski88 <boets003@mail.ru>
2018-11-04 19:55:49 +0300

    6.2. Перевернуть массив. [#4441][#82789]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -6,8 +6,10 @@ public class Turn {
     public int[] back(int[] array) {
     //public static void main(String args[]){
 
-       for (int i = 0; i <= array.length / 2; i++) {
-array[i] = array[array.length - 1];
+       for (int i = 0; i < array.length / 2; i++) {
+           int temp = array[i];
+array[i] = array[array.length - 1 - i];
+array[array.length -1 -i] = temp;
 //           System.out.println(array[i]);
 //           System.out.println(array.length - 1);
 
@@ -16,6 +18,11 @@ array[i] = array[array.length - 1];
         //System.out.println(Array.a.array);
 
     }
+//for (int i = 0; i <= array.length / 2; i++) { почему  <= ??? должно быть <
+//        нет переменной для сохранения перекидываемого значения
+//        int temp = array[i];
+//        array[i] = array[array.length - 1 - i];
+//        array[array.length - 1 - i] = temp;
 
 ////3. Метод должен перевернуть массив задом наперёд
 ////        4. В задании нельзя использовать дополнительный массив.
