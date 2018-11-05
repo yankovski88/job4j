9932cb24ba2838dba4c1836d69075f259d49f4b6
yankovski88 <boets003@mail.ru>
2018-11-06 00:32:11 +0300

    6.3. Массив заполнен true или false [#53857]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,21 +1,29 @@
 package ru.job4j.array;
 
 public class Check {
+    /**
+     * Check.mono(boolean[] data) при добавлении массива boolean он показывает или все элименты одинаковы
+     * @param data массив в котором будет находиться объект
+     * @author yura yankovski(mailto:boets003@mail.ru)
+     * @version 1.
+     */
 
-
-    public static void mono(boolean[] data) {
-        boolean result = false;
-        for (int i = 0; i <= data.length; i++) {
-            if(data[i] = false){
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++) {
+            boolean a = data[0];
+            if (a != data[i]) {
+                result = false;
+                break;
+            } else {
                 result = true;
-            }else if (data[i] = true){
-                result = true;
-            }else {result = false;}
-        }System.out.println(result);
-        }
-        ///return result;
-    public static void main(String args[]){
-        boolean[] data = new boolean[] {true};
-        Check.mono(data);
+            }
+        }//System.out.println(result);
+        return result;
     }
+
+//    public static void main(String args[]){
+//        boolean[] data = new boolean[] {false, false, false};
+//        Check.mono(data);
+//    }
 }
