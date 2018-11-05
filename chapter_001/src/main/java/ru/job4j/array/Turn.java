3d2b5fd31ad0aeb6af16b453576b1dbcc8feaada
yankovski88 <boets003@mail.ru>
2018-11-05 23:42:18 +0300

    6.2. Перевернуть массив. [#4441][#82789]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -10,20 +10,10 @@ public class Turn {
            int temp = array[i];
 array[i] = array[array.length - 1 - i];
 array[array.length -1 -i] = temp;
-//           System.out.println(array[i]);
-//           System.out.println(array.length - 1);
+
 
 } return array;
-      }//
+      }
         //System.out.println(Array.a.array);
 
     }
-//for (int i = 0; i <= array.length / 2; i++) { почему  <= ??? должно быть <
-//        нет переменной для сохранения перекидываемого значения
-//        int temp = array[i];
-//        array[i] = array[array.length - 1 - i];
-//        array[array.length - 1 - i] = temp;
-
-////3. Метод должен перевернуть массив задом наперёд
-////        4. В задании нельзя использовать дополнительный массив.
-////        5. В классе TurnTest напишите 2 тест-метода, которые будет проверять вашу программу для чётного и нечётного количества элементов в массиве.
